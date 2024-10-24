87a85fecea9887535a89de024af0355658a7269e
Matthew Bowen <matthew.bowen1989@protonmail.com>
2024-10-24 16:23:38 +0900

    Fix Node::toString() return type declaration

--- mecab/mecab7.c
+++ mecab/mecab7.c
@@ -379,7 +379,7 @@ static zend_function_entry mecab_methods[] = {
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_getIterator, 0, 0, Traversable, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry mecab_node_methods[] = {
